f82ecd2029e66f2e360505487abbe96b282437cb
TheCSDev <thecsdev@gmail.com>
2023-03-10 06:36:27 +0100

    Release v2.4+fabric

--- tcdcommons-1.19.2/src/main/java/io/github/thecsdev/tcdcommons/client/mixin/hooks/MixinButtonWidget.java
+++ tcdcommons-1.19.2/src/main/java/io/github/thecsdev/tcdcommons/client/mixin/hooks/MixinButtonWidget.java
@@ -1,6 +1,7 @@
 package io.github.thecsdev.tcdcommons.client.mixin.hooks;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import net.minecraft.client.gui.widget.ButtonWidget;
@@ -9,9 +10,11 @@ import net.minecraft.client.gui.widget.ButtonWidget.PressAction;
 @Mixin(ButtonWidget.class)
 public interface MixinButtonWidget
 {
+	@Mutable
 	@Accessor("onPress")
 	public abstract PressAction getOnPress();
 	
+	@Mutable
 	@Accessor("onPress")
 	public abstract void setOnPress(PressAction onPress);
 }
