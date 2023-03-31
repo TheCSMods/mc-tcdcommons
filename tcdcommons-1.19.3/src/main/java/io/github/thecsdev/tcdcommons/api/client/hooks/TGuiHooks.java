9890a15e696c78dad43d47e11bdf4d76e622518c
TheCSDev <thecsdev@gmail.com>
2023-03-31 16:53:48 +0200

    More port work with fabric-1.19.4

--- tcdcommons-1.19.3/src/main/java/io/github/thecsdev/tcdcommons/api/client/hooks/TGuiHooks.java
+++ tcdcommons-1.19.3/src/main/java/io/github/thecsdev/tcdcommons/api/client/hooks/TGuiHooks.java
@@ -1,7 +1,13 @@
 package io.github.thecsdev.tcdcommons.api.client.hooks;
 
+import java.util.List;
+import java.util.Objects;
+
 import io.github.thecsdev.tcdcommons.client.mixin.hooks.MixinButtonWidget;
+import io.github.thecsdev.tcdcommons.client.mixin.hooks.MixinGridWidget;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.gui.widget.GridWidget;
 
 public final class TGuiHooks
 {
@@ -27,6 +33,28 @@ public final class TGuiHooks
 	public static void setButtonPressAction(ButtonWidget button, ButtonWidget.PressAction pressAction)
 	{
 		((MixinButtonWidget)button).setOnPress(pressAction);
+	}
+	// ==================================================
+	/**
+	 * Returns the child {@link Widget}s of a given {@link GridWidget}.
+	 * @param grid The target {@link GridWidget}.
+	 * @throws NullPointerException When an argument is null.
+	 */
+	public static List<ClickableWidget> getGridWidgetChildren(GridWidget grid)
+	{
+		return ((MixinGridWidget)grid).getChildren();
+	}
+	
+	/**
+	 * Sets the child {@link Widget}s of a given {@link GridWidget}.
+	 * @param grid The target {@link GridWidget}.
+	 * @param widgets The new widget {@link List} to assign.
+	 * @throws NullPointerException When an argument is null.
+	 */
+	public static void setGridWidgetChildren(GridWidget grid, List<ClickableWidget> widgets)
+	{
+		Objects.requireNonNull(widgets);
+		((MixinGridWidget)grid).setChildren(widgets);
 	}
 	// ==================================================
 }
