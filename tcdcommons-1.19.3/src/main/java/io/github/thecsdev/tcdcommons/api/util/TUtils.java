20a58696d91f303a20ae51c0461551765b8d120c
TheCSDev <thecsdev@gmail.com>
2023-05-29 15:14:33 +0200

    Removed embedded dependencies
    This mod not requires manually installing them, therefore letting the user and/or other mods pick their own versions of choice for those dependencies.

---
+++ tcdcommons-1.19.3/src/main/java/io/github/thecsdev/tcdcommons/api/util/TUtils.java
@@ -0,0 +1,22 @@
+package io.github.thecsdev.tcdcommons.api.util;
+
+import java.util.UUID;
+
+import net.minecraft.util.Uuids;
+
+public final class TUtils
+{
+	// ==================================================
+	protected TUtils() {}
+	// ==================================================
+	/**
+	 * Returns the <b>offline</b> {@link UUID} for a given player nickname.<br/>
+	 * This {@link UUID} will not work for referencing "online" players.
+	 * @param nickname The player nickname.
+	 */
+	public static UUID getOfflinePlayerUuid(String nickname)
+	{
+		return Uuids.getOfflinePlayerUuid(nickname);
+	}
+	// ==================================================
+}
