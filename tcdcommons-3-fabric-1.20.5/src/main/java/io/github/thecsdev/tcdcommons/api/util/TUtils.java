93a01faaa6d331fcf84b57fa322ec55745aca5d9
Dave <thecsdev@gmail.com>
2024-03-08 13:03:46 +0100

    v3.9.2 dev. progress
    Bug fix and a utility method.

--- tcdcommons-3-fabric-1.20.5/src/main/java/io/github/thecsdev/tcdcommons/api/util/TUtils.java
+++ tcdcommons-3-fabric-1.20.5/src/main/java/io/github/thecsdev/tcdcommons/api/util/TUtils.java
@@ -1,8 +1,11 @@
 package io.github.thecsdev.tcdcommons.api.util;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.Nullable;
 
 import io.github.thecsdev.tcdcommons.api.util.io.mod.ModInfoProvider;
 import net.minecraft.util.Uuids;
@@ -44,7 +47,29 @@ public final class TUtils
 	public static String getModName(String modId)
 	{
 		if(StringUtils.isAllBlank(modId)) return "*";
-		return ModInfoProvider.getInstance().getModInfo(modId).getName().getString();
+		final @Nullable var mi = ModInfoProvider.getInstance().getModInfo(modId);
+		if(mi != null) return mi.getName().getString();
+		else return modId;
+	}
+	// --------------------------------------------------
+	/**
+	 * A safer implementation of {@link List#subList(int, int)}.<br/>
+	 * Credit: https://stackoverflow.com/a/31003453
+	 * @param list The {@link List} to sub-list from.
+	 * @param fromIndex Low endpoint (inclusive) of the subList.
+	 * @param toIndex High endpoint (exclusive) of the subList.
+	 */
+	public static <T> List<T> safeSubList(List<T> list, int fromIndex, int toIndex)
+	{
+		final int size = list.size();
+		if (fromIndex >= size || toIndex <= 0 || fromIndex >= toIndex)
+			return Collections.emptyList();
+		
+		fromIndex = Math.max(0, fromIndex);
+		toIndex = Math.min(size, toIndex);
+		
+		try { return list.subList(fromIndex, toIndex); }
+		catch(Exception e) { return Collections.emptyList(); }
 	}
 	// ==================================================
 }
